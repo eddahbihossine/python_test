8aa99dcac84df0ba79c169f34bfcc23443e2e95d
hossine eddahbi <eddahbi.hossine.solicode@gmail.com>
2023-12-04 15:42:20 +0100

    print out a message when the num are clear

---
+++ script.py
@@ -0,0 +1,28 @@
+import itertools
+import subprocess
+
+numbers = [1, 2, 3, 4]
+
+permutations = list(itertools.permutations(numbers))
+for permutation in permutations:
+    perm_str = ' '.join(map(str, permutation))
+
+    result = subprocess.run(['./push_swap', perm_str], capture_output=True, text=True)
+
+    num_instructions = len(result.stdout.split('\n')) - 1
+    l =  0
+
+    if num_instructions > 12:
+        print(f"Permutation: {perm_str}")
+        print(f"Number of instructions: {num_instructions}")
+        print("--------------------")
+        l = l + 1
+    else:
+        print ("all good")
+
+    
+    if l == 0:
+        print ("all good")
+
+    
+    
